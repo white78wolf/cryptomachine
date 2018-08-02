946b60ab60a8784a19e400f3fca94f9a6664aca7
White78Wolf <cepkov78@mail.ru>
2018-08-02 11:03:18 +0300

    First commit

---
+++ DemoEncrypter/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace DemoEncrypter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tsmiFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiClipboard = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiClear = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbOut = new System.Windows.Forms.TextBox();
+            this.btnAction = new System.Windows.Forms.Button();
+            this.lblCaption = new System.Windows.Forms.Label();
+            this.tbKey = new System.Windows.Forms.TextBox();
+            this.rtbIn = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiFile,
+            this.tsmiHelp});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tsmiFile
+            // 
+            this.tsmiFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiOpen,
+            this.tsmiClipboard,
+            this.tsmiClear,
+            this.tsmiExit});
+            this.tsmiFile.Name = "tsmiFile";
+            this.tsmiFile.Size = new System.Drawing.Size(48, 20);
+            this.tsmiFile.Text = "Файл";
+            // 
+            // tsmiOpen
+            // 
+            this.tsmiOpen.Name = "tsmiOpen";
+            this.tsmiOpen.Size = new System.Drawing.Size(242, 22);
+            this.tsmiOpen.Text = "Открыть файл";
+            this.tsmiOpen.Click += new System.EventHandler(this.tsmiOpen_Click);
+            // 
+            // tsmiClipboard
+            // 
+            this.tsmiClipboard.Name = "tsmiClipboard";
+            this.tsmiClipboard.Size = new System.Drawing.Size(242, 22);
+            this.tsmiClipboard.Text = "Копировать результат в буфер";
+            this.tsmiClipboard.Click += new System.EventHandler(this.tsmiClipboard_Click);
+            // 
+            // tsmiClear
+            // 
+            this.tsmiClear.Name = "tsmiClear";
+            this.tsmiClear.Size = new System.Drawing.Size(242, 22);
+            this.tsmiClear.Text = "Очистить поля";
+            this.tsmiClear.Click += new System.EventHandler(this.tsmiClear_Click);
+            // 
+            // tsmiExit
+            // 
+            this.tsmiExit.Name = "tsmiExit";
+            this.tsmiExit.Size = new System.Drawing.Size(242, 22);
+            this.tsmiExit.Text = "Выход";
+            this.tsmiExit.Click += new System.EventHandler(this.tsmiExit_Click);
+            // 
+            // tsmiHelp
+            // 
+            this.tsmiHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiAbout});
+            this.tsmiHelp.Name = "tsmiHelp";
+            this.tsmiHelp.Size = new System.Drawing.Size(65, 20);
+            this.tsmiHelp.Text = "Справка";
+            // 
+            // tsmiAbout
+            // 
+            this.tsmiAbout.Name = "tsmiAbout";
+            this.tsmiAbout.Size = new System.Drawing.Size(149, 22);
+            this.tsmiAbout.Text = "О программе";
+            this.tsmiAbout.Click += new System.EventHandler(this.tsmiAbout_Click);
+            // 
+            // tbOut
+            // 
+            this.tbOut.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tbOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.tbOut.Location = new System.Drawing.Point(0, 304);
+            this.tbOut.Multiline = true;
+            this.tbOut.Name = "tbOut";
+            this.tbOut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbOut.Size = new System.Drawing.Size(784, 257);
+            this.tbOut.TabIndex = 4;
+            // 
+            // btnAction
+            // 
+            this.btnAction.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnAction.Location = new System.Drawing.Point(12, 259);
+            this.btnAction.Name = "btnAction";
+            this.btnAction.Size = new System.Drawing.Size(358, 39);
+            this.btnAction.TabIndex = 3;
+            this.btnAction.Text = "Преобразовать";
+            this.btnAction.UseVisualStyleBackColor = true;
+            this.btnAction.Click += new System.EventHandler(this.btnAction_Click);
+            // 
+            // lblCaption
+            // 
+            this.lblCaption.AutoSize = true;
+            this.lblCaption.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblCaption.Location = new System.Drawing.Point(470, 272);
+            this.lblCaption.Name = "lblCaption";
+            this.lblCaption.Size = new System.Drawing.Size(158, 20);
+            this.lblCaption.TabIndex = 4;
+            this.lblCaption.Text = "Ключ шифрования:";
+            // 
+            // tbKey
+            // 
+            this.tbKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.tbKey.Location = new System.Drawing.Point(634, 266);
+            this.tbKey.Name = "tbKey";
+            this.tbKey.Size = new System.Drawing.Size(138, 29);
+            this.tbKey.TabIndex = 2;
+            this.tbKey.Text = "12";
+            // 
+            // rtbIn
+            // 
+            this.rtbIn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.rtbIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rtbIn.Location = new System.Drawing.Point(0, 24);
+            this.rtbIn.Name = "rtbIn";
+            this.rtbIn.Size = new System.Drawing.Size(784, 229);
+            this.rtbIn.TabIndex = 1;
+            this.rtbIn.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.rtbIn);
+            this.Controls.Add(this.tbKey);
+            this.Controls.Add(this.lblCaption);
+            this.Controls.Add(this.btnAction);
+            this.Controls.Add(this.tbOut);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Enigma v1.0";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFile;
+        private System.Windows.Forms.ToolStripMenuItem tsmiClear;
+        private System.Windows.Forms.ToolStripMenuItem tsmiExit;
+        private System.Windows.Forms.ToolStripMenuItem tsmiHelp;
+        private System.Windows.Forms.ToolStripMenuItem tsmiAbout;
+        private System.Windows.Forms.TextBox tbOut;
+        private System.Windows.Forms.Button btnAction;
+        private System.Windows.Forms.Label lblCaption;
+        private System.Windows.Forms.TextBox tbKey;
+        private System.Windows.Forms.ToolStripMenuItem tsmiOpen;
+        private System.Windows.Forms.RichTextBox rtbIn;
+        private System.Windows.Forms.ToolStripMenuItem tsmiClipboard;
+    }
+}
+
---
+++ DemoEncrypter/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+//using Microsoft.Office.Interop.Word;
+using Code7248.word_reader; // Может и .doc тоже
+
+namespace DemoEncrypter
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        // Кнопка "Выход"
+        private void tsmiExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        // Кнопка "Очистить поля"
+        private void tsmiClear_Click(object sender, EventArgs e)
+        {
+            rtbIn.Text = String.Empty;
+            tbOut.Text = String.Empty;
+            tbKey.Text = "12";
+        }
+        // Справка -> О программе
+        private void tsmiAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "Автор: Андерс Хейлсберг, \nДата релиза: январь 2002 г.",
+                "Enigma v 1.0",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+                );
+        }
+        // Кнопка "Преобразовать"
+        private void btnAction_Click(object sender, EventArgs e)
+        {
+            int key;
+
+            Int32.TryParse(tbKey.Text, out key);
+            tbOut.Text = Encryption(rtbIn.Text, key);            
+        }
+        // Метод зашифровки/дешифровки по ключу на основе побитовых операций
+        private string Encryption(string p1, int p2)
+        {
+            string temp = String.Empty;
+
+            foreach (char c in p1)
+            {
+                temp += Convert.ToString((char)(((int)(c) ^ p2))); // Магия строгой дизьюнкции (XOR)
+            }
+
+            return temp;
+        }
+        // Заморочился с возможностью открывать текстовые файлы для зашифровки содержимого (без сохранения обратно, хех)
+        private void tsmiOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            // Фильтруем расширения для отображения текстовых файлов
+            openFile.Filter = "Все текстовые файлы|*.txt;*rtf;*.doc;*docx;|Файлы .txt|*.txt|Файлы .rtf|*.rtf|Файлы .doc|*.doc|Файлы .docx|*.docx";
+            
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                string type = Path.GetExtension(openFile.FileName); // Извлекаем расширение
+                switch (type)
+                {
+                    case ".txt":
+                        try
+                        {
+                            rtbIn.Text = File.ReadAllText(openFile.FileName, Encoding.Default);                            
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Ошибка загрузки");
+                        }
+                        break;
+
+                    case ".rtf":
+                        try
+                        {
+                            rtbIn.LoadFile(openFile.FileName, RichTextBoxStreamType.RichText);
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Ошибка загрузки");
+                        }
+                        break;
+
+                    case ".doc" : // Работа с Interop.Word заставила задуматься, но вот чудо...
+                    case ".docx": // ...случайно нашёл в сети библиотеку Code7248.word_reader.dll
+                        try
+                        {
+                            TextExtractor extractor = new TextExtractor(openFile.FileName);
+                            string contents = extractor.ExtractText();
+                            rtbIn.Text = contents;
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Ошибка загрузки");
+                        }                        
+                        break;
+                }
+            }            
+        }
+        // Сохранение результата в буфер
+        private void tsmiClipboard_Click(object sender, EventArgs e)
+        {            
+            if (tbOut.Text == "")
+                Clipboard.Clear();
+            Clipboard.SetText(tbOut.Text);
+        }
+        // Фокус на текстбокс с исходным текстом
+        //private void MainForm_Shown(object sender, EventArgs e)
+        //{
+        //    rtbIn.Focus();
+        //}
+    }
+}
